8c776d13e71b0601250a0ffbd7ba578a418e5c30
Alexey Demin <AlexeyDeminA@gmail.com>
2024-09-08 19:50:47 +0300

    add matrix class

---
+++ src/math/matrix.cpp
@@ -0,0 +1,470 @@
+#include "matrix.h"
+
+#include <stdexcept>
+
+namespace NeuralNetwork::Math
+{
+	template<typename T>
+	Matrix<T>::Matrix(int rows, int cols, bool fillZero) : _rows(rows), _cols(cols), _matrix(nullptr)
+	{
+		AllocMatrix();
+
+		if (fillZero)
+			Fill(0);
+	}
+
+	template<typename T>
+	Matrix<T>::Matrix(int rows, int cols, T** data) : _rows(rows), _cols(cols), _matrix(nullptr)
+	{
+		AllocMatrix();
+
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				_matrix[row][col] = data[row][col];
+			}
+		}
+	}
+
+	template<typename T>
+	Matrix<T>::Matrix(std::initializer_list<std::initializer_list<T>> init_list) : _matrix(nullptr)
+	{
+		_rows = init_list.size();
+		_cols = init_list.begin()->size();
+		AllocMatrix();
+
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+
+				_matrix[row][col] = *((init_list.begin() + row)->begin() + col);
+			}
+		}
+	}
+
+	template<typename T>
+	Matrix<T>& Matrix<T>::operator=(const std::initializer_list<std::initializer_list<T>>& init_list)
+	{
+		int new_rows = init_list.size();
+		int new_cols = init_list.begin()->size();
+		if (new_rows != _rows || new_cols != _cols)
+		{
+			_rows = new_rows;
+			_cols = new_cols;
+			AllocMatrix();
+		}
+
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+
+				_matrix[row][col] = *((init_list.begin() + row)->begin() + col);
+			}
+		}
+	}
+
+	template<typename T>
+	Matrix<T>::Matrix(const Matrix& other) : _rows(other._rows), _cols(other._cols), _matrix(nullptr)
+	{
+		AllocMatrix();
+
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				_matrix[row][col] = other._matrix[row][col];
+			}
+		}
+	}
+	
+	template<typename T>
+	Matrix<T>::Matrix(Matrix&& other) noexcept : _rows(other._rows), _cols(other._cols), _matrix(other._matrix)
+	{
+		other._rows = 0;
+		other._cols = 0;
+		other._matrix = nullptr;
+	}
+
+	template<typename T>
+	Matrix<T>::~Matrix()
+	{
+		FreeMatrix();
+	}
+	
+	template<typename T>
+	Matrix<T> Matrix<T>::GetIdentity(int rank)
+	{
+		Matrix<T> outMatrix(rank, rank);
+		for (int k = 0; k < rank; k++)
+		{
+			outMatrix._matrix[k][k] = 1;
+		}
+		return outMatrix;
+	}
+
+	template<typename T>
+	Matrix<T> Matrix<T>::HadamardProduct(const Matrix<T>& other) const
+	{
+		if (_rows != other._rows || _cols != other._cols)
+			throw std::invalid_argument("Rows and Columns not equal");
+
+		Matrix<T> outMatrix;
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				outMatrix[row][col] = _matrix[row][col] * other._matrix[row][col];
+			}
+		}
+		return outMatrix;
+	}
+
+	template<typename T>
+	Matrix<T>& Matrix<T>::HadamardProductThis(const Matrix<T>& other)
+	{
+		if (_rows != other._rows || _cols != other._cols)
+			throw std::invalid_argument("Rows and Columns not equal");
+
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				_matrix[row][col] *= other._matrix[row][col];
+			}
+		}
+		return *this;
+	}
+
+	template<typename T>
+	int Matrix<T>::GetRows() const
+	{
+		return _rows;
+	}
+
+	template<typename T>
+	int Matrix<T>::GetCols() const
+	{
+		return _cols;
+	}
+
+	template<typename T>
+	Matrix<T>& Matrix<T>::Fill(T value)
+	{
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				_matrix[row][col] = value;
+			}
+		}
+		return *this;
+	}
+
+	template<typename T>
+	Matrix<T> Matrix<T>::Transpose() const
+	{
+		Matrix<T> outMatrix;
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				outMatrix._matrix[row][col] = _matrix[col][row];
+			}
+		}
+		return outMatrix;
+	}
+
+	template<typename T>
+	Matrix<T>& Matrix<T>::TransposeThis()
+	{
+		if (_rows != _cols)
+			throw std::invalid_argument("Matrix is not square");
+
+		for (int row = 0; row < _rows - 1; row++)
+		{
+			for (int col = row + 1; col < _cols; col++)
+			{
+				std::swap(_matrix[row][col], _matrix[col][row]);
+			}
+		}
+		return *this;
+	}
+
+	template<typename T>
+	T& Matrix<T>::operator()(int row, int col)
+	{
+		return _matrix[row][col];
+	}
+
+	template<typename T>
+	const T& Matrix<T>::operator()(int row, int col) const
+	{
+		return _matrix[row][col];
+	}
+
+	template<typename T>
+	Matrix<T>& Matrix<T>::operator=(const Matrix& other)
+	{
+		if (this == &other)
+			return *this;
+
+		_rows = other._rows;
+		_cols = other._cols;
+
+		AllocMatrix();
+
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				_matrix[row][col] = other._matrix[row][col];
+			}
+		}
+		return *this;
+	}
+
+	template<typename T>
+	Matrix<T>& Matrix<T>::operator=(Matrix&& other) noexcept
+	{
+		if (this == &other)
+			return *this;
+
+		FreeMatrix();
+		
+		_rows = other._rows;
+		_cols = other._cols;
+		_matrix = other._matrix;
+
+		other._rows = 0;
+		other._cols = 0;
+		other._matrix = nullptr;
+
+		return *this;
+	}
+
+	template<typename T>
+	Matrix<T> Matrix<T>::operator+(const Matrix<T>& other) const
+	{
+		if (_rows != other._rows || _cols != other._cols)
+			throw std::invalid_argument("Matrices must have the same dimensions for addition.");
+
+		Matrix<T> outMatrix(_rows, _cols, false);
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				outMatrix._matrix[row][col] = _matrix[row][col] + other._matrix[row][col];
+			}
+		}
+		return outMatrix;
+	}
+
+	template<typename T>
+	Matrix<T>& Matrix<T>::operator+=(const Matrix<T>& other)
+	{
+		if (_rows != other._rows || _cols != other._cols)
+			throw std::invalid_argument("Matrices must have the same dimensions for addition.");
+
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				_matrix[row][col] += other._matrix[row][col];
+			}
+		}
+		return *this;
+	}
+
+	template<typename T>
+	Matrix<T> Matrix<T>::operator-(const Matrix<T>& other) const
+	{
+		if (_rows != other._rows || _cols != other._cols)
+			throw std::invalid_argument("Matrices must have the same dimensions for substraction.");
+
+		Matrix<T> outMatrix(_rows, _cols, false);
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				outMatrix._matrix[row][col] = _matrix[row][col] - other._matrix[row][col];
+			}
+		}
+		return outMatrix;
+	}
+
+	template<typename T>
+	Matrix<T>& Matrix<T>::operator-=(const Matrix<T>& other)
+	{
+		if (_rows != other._rows || _cols != other._cols)
+			throw std::invalid_argument("Matrices must have the same dimensions for substraction.");
+
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				_matrix[row][col] -= other._matrix[row][col];
+			}
+		}
+		return *this;
+	}
+
+	template<typename T>
+	Matrix<T> Matrix<T>::operator*(const Matrix<T>& other) const
+	{
+		if (_cols != other._rows)
+			throw std::invalid_argument("The number of columns of the left matrix must be equal to the number of rows of the right matrix for multiplication.");
+
+		Matrix<T> outMatrix(_rows, other._cols, false);
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < other._cols; col++)
+			{
+				T sum = 0;
+				for (int k = 0; k < _cols; k++)
+				{
+					sum += _matrix[row][k] * other._matrix[k][col];
+				}
+				outMatrix[row][col] += sum;
+			}
+		}
+		return outMatrix;
+	}
+
+	template<typename T>
+	Matrix<T> Matrix<T>::operator*(T value) const
+	{
+		Matrix<T> outMatrix(*this);
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				outMatrix._matrix[row][col] *= value;
+			}
+		}
+		return outMatrix;
+	}
+
+	template<typename T>
+	Matrix<T>& Matrix<T>::operator*=(T value)
+	{
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				_matrix[row][col] *= value;
+			}
+		}
+		return *this;
+	}
+
+	template<typename T>
+	Matrix<T> Matrix<T>::operator/(T value) const
+	{
+		Matrix<T> outMatrix(*this);
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				outMatrix._matrix[row][col] /= value;
+			}
+		}
+		return outMatrix;
+	}
+
+	template<typename T>
+	Matrix<T>& Matrix<T>::operator/=(T value)
+	{
+		for (int row = 0; row < _rows; row++)
+		{
+			for (int col = 0; col < _cols; col++)
+			{
+				_matrix[row][col] /= value;
+			}
+		}
+		return *this;
+	}
+
+	template<typename T>
+	void Matrix<T>::AllocMatrix()
+	{
+		if (_matrix != nullptr)
+			FreeMatrix();
+
+		_matrix = new T*[_rows];
+		for (int row = 0; row < _rows; row++)
+		{
+			_matrix[row] = new T[_cols];
+		}
+	}
+
+	template<typename T>
+	void Matrix<T>::FreeMatrix()
+	{
+		if (_matrix == nullptr)
+			return;
+
+		for (int row = 0; row < _rows; row++)
+		{
+			delete[] _matrix[row];
+		}
+		delete[] _matrix;
+
+		_matrix = nullptr;
+	}
+
+	template<typename T>
+	Matrix<T> operator*(T value, const Matrix<T>& rhv)
+	{
+		Matrix<T> outMatrix(rhv);
+		for (int row = 0; row < rhv._rows; row++)
+		{
+			for (int col = 0; col < rhv._cols; col++)
+			{
+				outMatrix._matrix[row][col] *= value;
+			}
+		}
+		return outMatrix;
+	}
+
+	template<typename T>
+	Matrix<T> operator/(T value, const Matrix<T>& rhv)
+	{
+		Matrix<T> outMatrix(rhv);
+		for (int row = 0; row < rhv._rows; row++)
+		{
+			for (int col = 0; col < rhv._cols; col++)
+			{
+				outMatrix._matrix[row][col] = value / outMatrix._matrix[row][col];
+			}
+		}
+		return outMatrix;
+	}
+
+	template<typename T>
+	std::ostream& operator<<(std::ostream& stream, const Matrix<T>& matrix)
+	{
+		for (int row = 0; row < matrix._rows; row++)
+		{
+			for (int col = 0; col < matrix._cols; col++)
+			{
+				stream << matrix._matrix[row][col];
+			}
+			stream << std::endl;
+		}
+	}
+
+	template<typename T>
+	std::istream& operator>>(std::istream& stream, Matrix<T>& matrix)
+	{
+		for (int row = 0; row < matrix._rows; row++)
+		{
+			for (int col = 0; col < matrix._cols; col++)
+			{
+				stream >> matrix._matrix[row][col];
+			}
+		}
+	}
+}
