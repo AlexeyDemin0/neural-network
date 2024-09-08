723f5e140c9314f1ede1646c527403ad443869ff
Alexey Demin <AlexeyDeminA@gmail.com>
2024-09-08 22:05:53 +0300

    replace tabs with spaces

--- src/math/matrix.h
+++ src/math/matrix.h
@@ -6,64 +6,64 @@
 
 namespace NeuralNetwork::Math
 {
-	template<typename T>
-	class Matrix
-	{
-	private:
-		int _rows;
-		int _cols;
-		T** _matrix;
+    template<typename T>
+    class Matrix
+    {
+    private:
+        int _rows;
+        int _cols;
+        T** _matrix;
 
-	public:
-		Matrix(int rows, int cols, bool fillZero = true);
-		Matrix(int rows, int cols, T** data);
-		Matrix(std::initializer_list<std::initializer_list<T>> init_list);
+    public:
+        Matrix(int rows, int cols, bool fillZero = true);
+        Matrix(int rows, int cols, T** data);
+        Matrix(std::initializer_list<std::initializer_list<T>> init_list);
 
-		Matrix<T>& operator=(const std::initializer_list<std::initializer_list<T>>& init_list);
-		Matrix(const Matrix<T>& other);
-		Matrix<T>& operator=(const Matrix<T>& other);
-		Matrix(Matrix<T>&& other) noexcept;
-		Matrix<T>& operator=(Matrix<T>&& other) noexcept;
+        Matrix<T>& operator=(const std::initializer_list<std::initializer_list<T>>& init_list);
+        Matrix(const Matrix<T>& other);
+        Matrix<T>& operator=(const Matrix<T>& other);
+        Matrix(Matrix<T>&& other) noexcept;
+        Matrix<T>& operator=(Matrix<T>&& other) noexcept;
 
-		~Matrix();
-		
-		static Matrix<T> GetIdentity(int rank);
+        ~Matrix();
+        
+        static Matrix<T> GetIdentity(int rank);
 
-		int GetRows() const;
-		int GetCols() const;
+        int GetRows() const;
+        int GetCols() const;
 
-		Matrix<T>& Fill(T value);
+        Matrix<T>& Fill(T value);
 
-		Matrix<T> HadamardProduct(const Matrix<T>& other) const;
-		Matrix<T>& HadamardProductThis(const Matrix<T>& other);
+        Matrix<T> HadamardProduct(const Matrix<T>& other) const;
+        Matrix<T>& HadamardProductThis(const Matrix<T>& other);
 
-		Matrix<T> Transpose() const;
-		Matrix<T>& TransposeThis();
+        Matrix<T> Transpose() const;
+        Matrix<T>& TransposeThis();
 
-		T& operator()(int row, int col);
-		const T& operator()(int row, int col) const;
+        T& operator()(int row, int col);
+        const T& operator()(int row, int col) const;
 
-		Matrix<T> operator+(const Matrix<T>& other) const;
-		Matrix<T>& operator+=(const Matrix<T>& other);
+        Matrix<T> operator+(const Matrix<T>& other) const;
+        Matrix<T>& operator+=(const Matrix<T>& other);
 
-		Matrix<T> operator-(const Matrix<T>& other) const;
-		Matrix<T>& operator-=(const Matrix<T>& other);
+        Matrix<T> operator-(const Matrix<T>& other) const;
+        Matrix<T>& operator-=(const Matrix<T>& other);
 
-		Matrix<T> operator*(const Matrix<T>& other) const;
-		Matrix<T> operator*(T value) const;
-		Matrix<T>& operator*=(T value);
+        Matrix<T> operator*(const Matrix<T>& other) const;
+        Matrix<T> operator*(T value) const;
+        Matrix<T>& operator*=(T value);
 
-		Matrix<T> operator/(T value) const;
-		Matrix<T>& operator/=(T value);
+        Matrix<T> operator/(T value) const;
+        Matrix<T>& operator/=(T value);
 
-		friend Matrix<T> operator*(T value, const Matrix<T>& rhv);
-		friend Matrix<T> operator/(T value, const Matrix<T>& rhv);
+        friend Matrix<T> operator*(T value, const Matrix<T>& rhv);
+        friend Matrix<T> operator/(T value, const Matrix<T>& rhv);
 
-		friend std::ostream& operator<<(std::ostream& stream, const Matrix<T>& matrix);
-		friend std::istream& operator>>(std::istream& stream, Matrix<T>& matrix);
+        friend std::ostream& operator<<(std::ostream& stream, const Matrix<T>& matrix);
+        friend std::istream& operator>>(std::istream& stream, Matrix<T>& matrix);
 
-	private:
-		void AllocMatrix();
-		void FreeMatrix();
-	};
+    private:
+        void AllocMatrix();
+        void FreeMatrix();
+    };
 }
