e9ce74e9803ae49fcd1d71d98fe9336dd7a662b1
Alexey Demin <AlexeyDeminA@gmail.com>
2024-09-10 22:42:49 +0300

    added explicit template instantiation for functions

--- src/math/functions.cpp
+++ src/math/functions.cpp
@@ -2,6 +2,8 @@
 #include "functions.h"
 #include <cmath>
 
+#define _NN_DECLFUNC(FUNCNAME) template float FUNCNAME<float>(float); template double FUNCNAME<double>(double);
+
 namespace NeuralNetwork::Math::Functions
 {
     template<typename T>
@@ -68,15 +70,30 @@ namespace NeuralNetwork::Math::Functions
         return static_cast<T>(1.0) - y * y;
     }
 
-    template<typename T, T alpha>
+    template<typename T>
     T ReLU(T x)
     {
-        return x < static_cast<T>(0.0) ? alpha * x : x;
+        return x < static_cast<T>(0.0) ? static_cast<T>(0.0) : x;
     }
 
-    template<typename T, T alpha>
+    template<typename T>
     T ReLUDerivative(T x)
     {
-        return x < static_cast<T>(0.0) ? alpha : static_cast<T>(1.0);
+        return x < static_cast<T>(0.0) ? static_cast<T>(0.0) : static_cast<T>(1.0);
     }
+
+    _NN_DECLFUNC(Linear);
+    _NN_DECLFUNC(LinearDerivative);
+    _NN_DECLFUNC(BinaryStep);
+    _NN_DECLFUNC(BinaryStepDerivative);
+    _NN_DECLFUNC(Sigmoid);
+    _NN_DECLFUNC(SigmoidDerivative);
+    _NN_DECLFUNC(SigmoidDerivativeOptimized);
+    _NN_DECLFUNC(HyperbolicTangent);
+    _NN_DECLFUNC(HyperbolicTangentDerivative);
+    _NN_DECLFUNC(HyperbolicTangentDerivativeOptimized);
+    _NN_DECLFUNC(ReLU);
+    _NN_DECLFUNC(ReLUDerivative);
 }
+
+#undef _NN_DECLFUNC;
