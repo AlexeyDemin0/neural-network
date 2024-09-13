55f60eef00ea12830d8cce5ddba8011df1b93048
Alexey Demin <AlexeyDeminA@gmail.com>
2024-09-13 03:25:37 +0300

    changed indexes

--- src/perceptron.cpp
+++ src/perceptron.cpp
@@ -19,9 +19,9 @@ namespace NeuralNetwork
         _bias.resize(neuronsCountPerLayer.size() - 1);
         for (int i = 0; i < neuronsCountPerLayer.size() - 1; i++)
         {
-            int neuronsCount = neuronsCountPerLayer[i];
+            int neuronsCountCurrent = neuronsCountPerLayer[i];
             int neuronsCountNext = neuronsCountPerLayer[i + 1];
-            _weights[i] = Math::Matrix<NN_TYPE>(neuronsCountNext, neuronsCount, false);
+            _weights[i] = Math::Matrix<NN_TYPE>(neuronsCountNext, neuronsCountCurrent, false);
             _bias[i] = Math::Matrix<NN_TYPE>(neuronsCountNext, 1, false);
         }
     }
@@ -81,13 +81,13 @@ namespace NeuralNetwork
             if (cacheAfterActivationFunction)
             {
                 _layers[i + 1].ApplyFunction(activationFunction);
-                _derivatives[i + 1] = _layers[i + 1];
-                _derivatives[i + 1].ApplyFunction(derivativeFunction);
+                _derivatives[i] = _layers[i + 1];
+                _derivatives[i].ApplyFunction(derivativeFunction);
             }
             else
             {
-                _derivatives[i + 1] = _layers[i + 1];
-                _derivatives[i + 1].ApplyFunction(derivativeFunction);
+                _derivatives[i] = _layers[i + 1];
+                _derivatives[i].ApplyFunction(derivativeFunction);
                 _layers[i + 1].ApplyFunction(activationFunction);
             }
         }
@@ -96,25 +96,24 @@ namespace NeuralNetwork
 
     void Perceptron::BackwardPropagation(const Math::Matrix<NN_TYPE>& idealValues, NN_TYPE learningRate)
     {
-        // Output layer
-        int layerIndex = _layers.size() - 1;
-        _deltas[layerIndex] = _layers[layerIndex];
+        int layerIndex = _layers.size() - 2;
+        _deltas[layerIndex] = _layers[layerIndex + 1];
         _deltas[layerIndex] -= idealValues;
         _deltas[layerIndex] *= static_cast<NN_TYPE>(2.0);
         _deltas[layerIndex].HadamardProductThis(_derivatives[layerIndex]);
 
-        Math::Matrix<NN_TYPE>::MultMatrixToTransposedAndStoreTo(_deltas[layerIndex], _layers[layerIndex - 1], _deltasWeights[layerIndex - 1]);
-        _deltasBias[layerIndex - 1] = _deltas[layerIndex];
+        Math::Matrix<NN_TYPE>::MultMatrixToTransposedAndStoreTo(_deltas[layerIndex], _layers[layerIndex], _deltasWeights[layerIndex]);
+        _deltasBias[layerIndex] = _deltas[layerIndex];
         layerIndex--;
 
         // Hidden layers
         for (; layerIndex > 0; layerIndex--)
         {
-            _deltas[layerIndex + 1].HadamardProductThis(_derivatives[layerIndex]);
-            Math::Matrix<NN_TYPE>::MultTransposedToMatrixAndStoreTo(_weights[layerIndex - 1], _deltas[layerIndex + 1], _deltas[layerIndex]);
+            Math::Matrix<NN_TYPE>::MultTransposedToMatrixAndStoreTo(_weights[layerIndex + 1], _deltas[layerIndex + 1], _deltas[layerIndex]);
+            _deltas[layerIndex].HadamardProductThis(_derivatives[layerIndex]);
 
-            Math::Matrix<NN_TYPE>::MultMatrixToTransposedAndStoreTo(_deltas[layerIndex], _layers[layerIndex - 1], _deltasWeights[layerIndex - 1]);
-            _deltasBias[layerIndex - 1] = _deltas[layerIndex];
+            Math::Matrix<NN_TYPE>::MultMatrixToTransposedAndStoreTo(_deltas[layerIndex], _layers[layerIndex], _deltasWeights[layerIndex]);
+            _deltasBias[layerIndex] = _deltas[layerIndex];
         }
 
         // Weights adjusting
@@ -136,22 +135,19 @@ namespace NeuralNetwork
         _cacheIsInitialized = true;
 
         int layersCount = _layers.size();
-        _derivatives.resize(layersCount);
-        _deltas.resize(layersCount);
+        _derivatives.resize(layersCount - 1);
+        _deltas.resize(layersCount - 1);
         _deltasWeights.resize(layersCount - 1);
         _deltasBias.resize(layersCount - 1);
 
-        for (int i = 0; i < layersCount; i++)
-        {
-            _derivatives[i] = Math::Matrix<NN_TYPE>(_layers[i].GetRows(), 1, false);
-            _deltas[i] = Math::Matrix<NN_TYPE>(_layers[i].GetRows(), 1, false);
-        }
-
         for (int i = 0; i < layersCount - 1; i++)
         {
-            int neuronsCount = _layers[i].GetRows();
+            int neuronsCountCurrent = _layers[i].GetRows();
             int neuronsCountNext = _layers[i + 1].GetRows();
-            _deltasWeights[i] = Math::Matrix<NN_TYPE>(neuronsCountNext, neuronsCount, false);
+
+            _derivatives[i] = Math::Matrix<NN_TYPE>(_layers[i + 1].GetRows(), 1, false);
+            _deltas[i] = Math::Matrix<NN_TYPE>(_layers[i + 1].GetRows(), 1, false);
+            _deltasWeights[i] = Math::Matrix<NN_TYPE>(neuronsCountNext, neuronsCountCurrent, false);
             _deltasBias[i] = Math::Matrix<NN_TYPE>(neuronsCountNext, 1, false);
         }
     }
